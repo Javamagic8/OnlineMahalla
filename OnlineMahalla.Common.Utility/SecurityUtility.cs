7954211687b01d8f1fd91c6e6e8379ce2bb69330
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-12-18 10:49:20 +0500

    Utility

---
+++ OnlineMahalla.Common.Utility/SecurityUtility.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMahalla.Common.Utility
+{
+    public class Crc32 : HashAlgorithm
+    {
+        public const UInt32 DefaultPolynomial = 0xedb88320;
+        public const UInt32 DefaultSeed = 0xffffffff;
+
+        private UInt32 hash;
+        private UInt32 seed;
+        private UInt32[] table;
+        private UInt32[] defaultTable;
+
+        public Crc32()
+        {
+            table = InitializeTable(DefaultPolynomial);
+            seed = DefaultSeed;
+            Initialize();
+        }
+
+        public Crc32(UInt32 polynomial, UInt32 seed)
+        {
+            table = InitializeTable(polynomial);
+            this.seed = seed;
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            hash = CalculateHash(table, hash, buffer, start, length);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            byte[] hashBuffer = UInt32ToBigEndianBytes(~hash);
+            this.HashValue = hashBuffer;
+            return hashBuffer;
+        }
+
+        public override int HashSize
+        {
+            get { return 32; }
+        }
+
+        public UInt32 Compute(byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(DefaultPolynomial), DefaultSeed, buffer, 0, buffer.Length);
+        }
+
+        public UInt32 Compute(UInt32 seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(DefaultPolynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        public UInt32 Compute(UInt32 polynomial, UInt32 seed, byte[] buffer)
+        {
+            return ~CalculateHash(InitializeTable(polynomial), seed, buffer, 0, buffer.Length);
+        }
+
+        private UInt32[] InitializeTable(UInt32 polynomial)
+        {
+            if (polynomial == DefaultPolynomial && defaultTable != null)
+                return defaultTable;
+
+            UInt32[] createTable = new UInt32[256];
+            for (int i = 0; i < 256; i++)
+            {
+                UInt32 entry = (UInt32)i;
+                for (int j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry = entry >> 1;
+                createTable[i] = entry;
+            }
+
+            if (polynomial == DefaultPolynomial)
+                defaultTable = createTable;
+
+            return createTable;
+        }
+
+        private UInt32 CalculateHash(UInt32[] table, UInt32 seed, byte[] buffer, int start, int size)
+        {
+            UInt32 crc = seed;
+            for (int i = start; i < size; i++)
+                unchecked
+                {
+                    crc = (crc >> 8) ^ table[buffer[i] ^ crc & 0xff];
+                }
+            return crc;
+        }
+
+        private byte[] UInt32ToBigEndianBytes(UInt32 x)
+        {
+            return new byte[] {
+            (byte)((x >> 24) & 0xff),
+            (byte)((x >> 16) & 0xff),
+            (byte)((x >> 8) & 0xff),
+            (byte)(x & 0xff)
+        };
+        }
+    }
+
+    public static class HashHelper
+    {
+        public static byte[] GetDataString(string DataForHash)
+        {
+            byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(DataForHash);
+            Com.Intsoft.Crypto.IDigest digest = new Com.Intsoft.Crypto.Digests.UZDST1106Digest();
+            digest.BlockUpdate(byteArray, 0, byteArray.Length);
+            byte[] RowDataHash = new byte[digest.GetDigestSize()];
+            digest.DoFinal(RowDataHash, 0);
+            return RowDataHash;
+        }
+        public static string GetDataCrc32(string DataForCrc32)
+        {
+            System.Text.ASCIIEncoding AsciiEncoding = new System.Text.ASCIIEncoding();
+            Crc32 crc32 = new Crc32();
+
+            string crc32info = "";
+            foreach (byte b in crc32.ComputeHash(AsciiEncoding.GetBytes(DataForCrc32))) crc32info += b.ToString("x2").ToUpper();
+            return crc32info;
+        }
+        public static string GetHashSHA1Base64(string Data)
+        {
+            var sha1 = SHA1.Create();
+
+            var hash = sha1.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Data));
+            return Convert.ToBase64String(hash);
+        }
+        public static string GetHashSHA1Hex(string Data)
+        {
+            var sha1 = SHA1.Create();
+            var hash = sha1.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Data));
+            return HexStringFromBytes(hash);
+        }
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString().ToUpper();
+        }
+        public static string CreateRandomSalt()
+        {
+            Byte[] saltBytes = new Byte[4];
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            rng.GetBytes(saltBytes);
+
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        /// <summary>
+        /// Compute salted password hash
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        public static string ComputeSaltedHash(string password, string salt)
+        {
+            // Create Byte array of password string
+            UnicodeEncoding encoder = new UnicodeEncoding();
+            Byte[] secretBytes = encoder.GetBytes(password);
+
+            // Create a new salt
+            Byte[] saltBytes = Convert.FromBase64String(salt);
+
+            // append the two arrays
+            Byte[] toHash = new Byte[secretBytes.Length + saltBytes.Length];
+            Array.Copy(secretBytes, 0, toHash, 0, secretBytes.Length);
+            Array.Copy(saltBytes, 0, toHash, secretBytes.Length, saltBytes.Length);
+
+            SHA512 sha512 = SHA512.Create();
+            Byte[] computedHash = sha512.ComputeHash(toHash);
+
+            return Convert.ToBase64String(computedHash);
+        }
+
+    }
+    public class CustomPaswordHasher
+    {
+        public string HashPassword(string password, string salt)
+        {
+            return HashHelper.ComputeSaltedHash(password, salt);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string providedPassword, string salt)
+        {
+            if (string.Equals(hashedPassword, HashHelper.ComputeSaltedHash(providedPassword, salt), StringComparison.Ordinal))
+                return true;
+            else
+                return false;
+        }
+    }
+}
