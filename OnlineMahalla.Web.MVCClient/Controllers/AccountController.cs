15e757bd1e0346d48d56785067d1382b75bd2502
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-12-20 15:15:44 +0500

    Account Controller

---
+++ OnlineMahalla.Web.MVCClient/Controllers/AccountController.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using OnlineMahalla.Common.Model.Interface;
+using OnlineMahalla.Web.MVCClient.Extentions;
+using System.Security.Claims;
+
+namespace OnlineMahalla.Web.MVCClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IDataRepository _dataRepository;
+        private readonly IStringLocalizer<AccountController> _localizer;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly string userip = "";
+        private readonly string useragent = "";
+        public AccountController(IDataRepository dataRepository, IStringLocalizer<AccountController> localizer, Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor, IWebHostEnvironment hostingEnvironment)
+        {
+            var username = httpContextAccessor.HttpContext.User.GetUserName();
+            _dataRepository = dataRepository;
+
+            var orgid = httpContextAccessor.HttpContext.User.GetOrganizationID();
+            _dataRepository.OrgID = orgid;
+            var ischildlogout = httpContextAccessor.HttpContext.User.GetIsChildLogOut();
+            _dataRepository.IsChildLogOut = ischildlogout;
+            _dataRepository.UserName = username;
+            userip = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            if (httpContextAccessor.HttpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
+                userip += ";" + httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"];
+            if (httpContextAccessor.HttpContext.Request.Headers.ContainsKey("User-Agent"))
+                useragent = httpContextAccessor.HttpContext.Request.Headers["User-Agent"];
+            _localizer = localizer;
+            _hostingEnvironment = hostingEnvironment;
+
+        }
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+
+            ViewData["Title"] = _localizer["AppTitle"];
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["Title"] = _localizer["AppTitle"];
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string userName, string password, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (!string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(password) && _dataRepository.ValidatePassword(userName, password, userip, useragent))
+            {
+                // issue authentication cookie with subject ID and username
+                var user = _dataRepository.GetUser(userName, userip, useragent);
+
+                //int organizationid = 0;
+                //if (_dataRepository.UserIsInRole("CentralAccountingChild", user.ID))
+                //{
+                //    organizationid = (user.TempOrganizationID != null && user.TempOrganizationID.Value != 0) ? user.TempOrganizationID.Value : user.OrganizationID;
+                //}
+                //else
+                //{
+                //    organizationid = user.OrganizationID;
+
+                //}
+
+                AuthenticationProperties props = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20),
+                };
+
+
+                var claims = new List<Claim>
+                {
+                new Claim("sub", user.Name),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim("name", user.DisplayName),
+                new Claim("userid", user.ID.ToString(),ClaimValueTypes.Integer),
+                new Claim("orgid", user.OrganizationID.ToString(),ClaimValueTypes.Integer),
+                new Claim("orginn", user.OrganizationINN),
+                new Claim("orgname", user.OrganizationName),
+                new Claim("temporgid", user.TempOrganizationID.ToString(),ClaimValueTypes.Integer),
+                new Claim("temporginn", user.TempOrganizationINN),
+                new Claim("temporgname", user.TempOrganizationName),
+                new Claim("ischildlogout", "true",ClaimValueTypes.Boolean),
+                };
+
+                var id = new ClaimsIdentity(claims, "password");
+                var p = new ClaimsPrincipal(id);
+
+                await HttpContext.SignInAsync("Cookies", p);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", _localizer["Неправильное имя пользователя или пароль!"].Value);
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SelectedOrg(int SelectedOrganizationID)
+        {
+            try
+            {
+                // _dataRepository.GetUserOrganizations(SelectedOrganizationID);
+
+                var user = _dataRepository.GetUser(User.GetUserName(), userip, useragent);
+                int organizationid = 0;
+                if (_dataRepository.UserIsInRole("CentralAccountingChild", user.ID))
+                {
+                    organizationid = SelectedOrganizationID;
+                }
+                else
+                {
+                    organizationid = user.OrganizationID;
+
+                }
+
+                AuthenticationProperties props = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20),
+                };
+
+                var claims = new List<Claim>
+                {
+                new Claim("sub", user.Name),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim("name", user.DisplayName),
+                new Claim("userid", user.ID.ToString(),ClaimValueTypes.Integer),
+                new Claim("orgid", organizationid.ToString(),ClaimValueTypes.Integer),
+                new Claim("orginn", user.OrganizationINN),
+                new Claim("orgname", user.OrganizationName),
+                new Claim("temporgid", SelectedOrganizationID.ToString(),ClaimValueTypes.Integer),
+                new Claim("temporginn", user.TempOrganizationINN),
+                new Claim("temporgname", user.TempOrganizationName),
+                new Claim("ischildlogout", "false",ClaimValueTypes.Boolean),
+                };
+
+                var id = new ClaimsIdentity(claims, "password");
+                var p = new ClaimsPrincipal(id);
+
+                await HttpContext.SignInAsync("Cookies", p);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(string oldpassword, string newpassword, string confirmedpassword)
+        {
+            var username = User.GetUserName();
+            if (oldpassword == newpassword)
+                ModelState.AddModelError("", _localizer["Старый и новый пароль одинаковы!"].Value);
+
+            else if (!string.IsNullOrWhiteSpace(oldpassword) && !string.IsNullOrWhiteSpace(newpassword) && newpassword == confirmedpassword && _dataRepository.ValidatePassword(username, oldpassword, userip, useragent))
+            {
+                _dataRepository.ChangePassword(username, oldpassword, newpassword, confirmedpassword, userip, useragent);
+                await HttpContext.SignOutAsync("Cookies");
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", _localizer["Неправильное имя пользователя или пароль!"].Value);// InvalidUserNameOrPassword
+
+            return View();
+        }
+        public async Task<IActionResult> LogOut()
+        {
+            if (_dataRepository.UserIsInRole("CentralAccountingChild"))
+                _dataRepository.GetClearUserOrganizations();
+
+            await HttpContext.SignOutAsync("Cookies");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
