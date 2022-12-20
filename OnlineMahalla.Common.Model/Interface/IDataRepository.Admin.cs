bc4a257104ef71ecd8ebda2861166e525a0cd29e
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-12-20 16:51:23 +0500

    iData repository

--- OnlineMahalla.Common.Model/Interface/IDataRepository.Admin.cs
+++ OnlineMahalla.Common.Model/Interface/IDataRepository.Admin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineMahalla.Common.Model.Models.sys;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +7,12 @@ using System.Threading.Tasks;
 
 namespace OnlineMahalla.Common.Model.Interface
 {
-    public  class IDataRepository
-    {
-        string UserName { get; set; }
-        string IpAdress { get; set; }
-        string UserAgent { get; set; }
+    
 
-        int OrgID { get; set; }
-        int OrganizationID { get; }
-        bool IsChildLogOut { get; set; }
-        User GetUser(string username, string ipadress, string useragent);
-        bool ValidatePassword(string username, string password, string ipadress, string useragent);
-        bool ChangePassword(string username, string oldpassword, string newpassword, string confirmedpassword, string ipadress, string useragent);
+    public class PagedDataEx
+    {
+        public IEnumerable<dynamic> rows { get; set; } = new List<dynamic>();
+        public dynamic totalRow { get; set; }
+        public int total { get; set; }
     }
 }
