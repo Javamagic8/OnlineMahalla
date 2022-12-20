f3e61479381b0450e4bcaeacd14047eb648b498e
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-12-20 15:16:03 +0500

    Admin repository

---
+++ OnlineMahalla.Data.Repository.SqlServer/DataRepository.Admin.cs
@@ -0,0 +1,13 @@
+﻿using OnlineMahalla.Common.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMahalla.Data.Repository.SqlServer
+{
+    public partial class DataRepository : IDataRepository
+    {
+    }
+}
