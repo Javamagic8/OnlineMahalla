2212905313833acca37680727193804f0827530a
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2022-12-20 15:15:24 +0500

    Admin Repository

---
+++ OnlineMahalla.Common.Model/Interface/IDataRepository.Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMahalla.Common.Model.Interface
+{
+    public  class IDataRepository
+    {
+        string UserName { get; set; }
+        string IpAdress { get; set; }
+        string UserAgent { get; set; }
+
+        int OrgID { get; set; }
+        int OrganizationID { get; }
+        bool IsChildLogOut { get; set; }
+        User GetUser(string username, string ipadress, string useragent);
+        bool ValidatePassword(string username, string password, string ipadress, string useragent);
+        bool ChangePassword(string username, string oldpassword, string newpassword, string confirmedpassword, string ipadress, string useragent);
+    }
+}
