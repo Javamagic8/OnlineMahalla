d59aea13af8d19703e831c0cf565f9f7b69d1f4b
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2023-03-07 18:06:50 +0500

    sa

--- OnlineMahalla.Data.Repository.SqlServer/sys/NationRepository.cs
+++ OnlineMahalla.Data.Repository.SqlServer/sys/NationRepository.cs
@@ -13,21 +13,29 @@ namespace OnlineMahalla.Data.Repository.SqlServer
             PagedDataEx data = new PagedDataEx();
 
             Dictionary<string, object> sqlparamas = new Dictionary<string, object>();
-            string sqlselect = "SELECT  * ";
-            string sqlfrom = " FROM hl_Nationality Nationality";
-            string sqlwhere = " WHERE rol.StateID=1";
+            string sqlselect = @"SELECT [ID]
+                                       ,[Name]
+                                       ,[DisplayName]
+                                       ,[StatusID]
+                                       ,[StateID]
+                                       ,[DateOfCreate]
+                                       ,[DateOfModified]
+                                       ,[CreateUserID]
+                                       ,[ModifiedUserID] ";
+            string sqlfrom = " FROM [Online_Mahalla].[dbo].[info_Nation] Nation";
+            string sqlwhere = " WHERE Nation.StateID=1";
 
             if (!String.IsNullOrEmpty(Name))
             {
-                sqlwhere += " AND (Nationality.Name like '%' + @Name + '%')";
+                sqlwhere += " AND (Nation.Name like '%' + @Name + '%')";
                 sqlparamas.Add("@Name", Name);
             }
-            string sqlcount = "SELECT Count(Nationality.ID) " + sqlfrom + sqlwhere;
+            string sqlcount = "SELECT Count(Nation.ID) " + sqlfrom + sqlwhere;
 
             if (!String.IsNullOrEmpty(Sort))
                 sqlwhere += " ORDER BY " + Sort + " " + (Order == "asc" ? " " : " DESC");
             else
-                sqlwhere += " ORDER BY Nationality.ID";
+                sqlwhere += " ORDER BY Nation.ID";
             string sql = sqlselect + sqlfrom + sqlwhere;
             data.rows = _databaseExt.GetDataFromSql(sql, sqlparamas);
             data.total = (int)_databaseExt.ExecuteScalar(sqlcount, sqlparamas);
@@ -35,13 +43,28 @@ namespace OnlineMahalla.Data.Repository.SqlServer
         }
         public Nation GetNation(int id)
         {
-            var data = _databaseExt.GetFirstDataFromSql(" SELECT * FROM hl_Nationality ", new string[] { }, new object[] { });
+            var data = _databaseExt.GetFirstDataFromSql(@" SELECT [ID]
+                                       ,[Name]
+                                       ,[DisplayName]
+                                       ,[StatusID]
+                                       ,[StateID]
+                                       ,[DateOfCreate]
+                                       ,[DateOfModified]
+                                       ,[CreateUserID]
+                                       ,[ModifiedUserID] 
+                            FROM [Online_Mahalla].[dbo].[info_Nation] Nation WHERE ID = @ID", new string[] {"@ID"}, new object[] {id});
 
             Nation nation = new Nation()
             {
                 ID = data.ID,
                 Name = data.Name,
-                DisplayName = data.DisplayName
+                DisplayName = data.DisplayName,
+                StatusID = data.StatusID,
+                StateID = data.StateID,
+                DateOfCreate = data.DateOfCreate,
+                DateOfModified = data.DateOfModified,
+                CreateUserID = data.CreateUserID,
+                ModifiedUserID = data.ModifiedUserID
             };
             return nation;
         }
@@ -54,28 +77,32 @@ namespace OnlineMahalla.Data.Repository.SqlServer
             myConn.Open();
             using var ts = myConn.BeginTransaction();
 
-            string sql = "SELECT ID FROM [dbo].[hl_Nation] WHERE ID<>@ID AND Name=@Name";
+            string sql = "SELECT [ID] FROM [info_Nation]  WHERE ID <> @ID AND [Name] = @Name";
 
             var checkexists = _databaseExt.ExecuteScalar(sql,
                 new string[] { "@ID", "@Name" },
                 new object[] { nation.ID, nation.Name }, System.Data.CommandType.Text, ts);
             if (checkexists != null && checkexists != DBNull.Value)
-                throw new Exception("Рол уже добавлен. " + nation.Name);
+                throw new Exception("Нация уже добавлен. " + nation.Name);
             //FinancialAuthority
             if (nation.ID == 0)
             {
-                sql = "INSERT INTO [dbo].[sys_Role] ([Name],[StateID],[CreatedUserID],[DateOfCreated]) VALUES (@Name,@StateID,@CreatedUserID,GETDATE()) select [ID] from sys_Role where @@ROWCOUNT > 0 and [ID] = scope_identity()";
+                sql = @"INSERT INTO [dbo].[info_Nation]
+                                   ([Name],[DisplayName],[CreateUserID])
+                             VALUES
+                                   (@Name, @DisplayName, @UserID)
+	                        	   select [ID] from [info_Nation] where @@ROWCOUNT > 0 and [ID] = scope_identity()";
                 var NewID = _databaseExt.ExecuteScalar(sql,
-                     new string[] { "@Name", "@StateID", "@CreatedUserID" },
-                     new object[] { nation.Name, 1, UserID }, System.Data.CommandType.Text, ts);
+                     new string[] { "@Name", "@DisplayName", "@UserID" },
+                     new object[] { nation.Name, nation.DisplayName, UserID }, System.Data.CommandType.Text, ts);
                 nation.ID = Convert.ToInt32(NewID);
             }
             else
             {
-                sql = "UPDATE [dbo].[sys_Role] SET [Name] = @Name,[StateID]=@StateID ,[ModifiedUserID] = @ModifiedUserID  WHERE ID=@ID";
+                sql = "UPDATE [dbo].[info_Nation] SET [Name] = @Name, [DisplayName] = @DisplayName ,[StateID]=@StateID ,[ModifiedUserID] = @ModifiedUserID, StatusID = @StatusID, DateOfModified = GETDATE() WHERE ID=@ID ";
                 _databaseExt.ExecuteNonQuery(sql,
-                    new string[] { "@Name", "@StateID", "@ModifiedUserID", "@ID" },
-                    new object[] { nation.Name, 1, UserID, nation.ID }, System.Data.CommandType.Text, ts);
+                    new string[] { "@Name", "@DisplayName", "@StateID", "@ModifiedUserID", "@StatusID", "@ID" },
+                    new object[] { nation.Name, nation.DisplayName,nation.StateID, UserID, nation.StatusID,nation.ID }, System.Data.CommandType.Text, ts);
             }
             ts.Commit();
         }
@@ -86,9 +113,7 @@ namespace OnlineMahalla.Data.Repository.SqlServer
                 myConn.Open();
                 using (var ts = myConn.BeginTransaction())
                 {
-                    _databaseExt.ExecuteNonQuery("DELETE FROM sys_RoleModule WHERE RoleID=@ID", new string[] { "@ID" }, new object[] { id }, System.Data.CommandType.Text, ts);
-                    _databaseExt.ExecuteNonQuery("DELETE FROM sys_Role WHERE ID=@ID", new string[] { "@ID" }, new object[] { id }, System.Data.CommandType.Text, ts);
-                    ts.Commit();
+                    _databaseExt.ExecuteNonQuery("UPDATE info_Nation set StateID = 2 WHERE ID=@ID", new string[] { "@ID" }, new object[] { id }, System.Data.CommandType.Text, ts);
                 }
             }
         }
