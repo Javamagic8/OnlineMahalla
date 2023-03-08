c1b83ae06f0a637b14c6d1f11ab061e18ac7f202
Javamagic8 <74010882+Javamagic8@users.noreply.github.com>
2023-03-09 00:29:27 +0500

    sa

---
+++ OnlineMahalla.Web.MVCClient/Controllers/CityzenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineMahalla.Web.MVCClient.Controllers
+{
+    public class CityzenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
